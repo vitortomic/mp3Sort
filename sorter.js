d824a3e8e69b7eee4c86c482bda6e50bd056645d
vitortomic <vitortomic@gmail.com>
2019-11-26 16:47:46 +0100

    implemented sorter

--- sorter.js
+++ sorter.js
@@ -3,28 +3,36 @@ const fs = require('fs');
 const mm = require('music-metadata')
 
 const mp3Regex = new RegExp(".mp3$")
+let counter = 0
 
-const processFile = async (file)=>{
+const processFile = async (file, key, array)=>{
   const filePath = `${config.readPath}${file}`
   const tags = (await mm.parseFile(filePath)).common
-  console.log(file)
-  console.log(tags.artist)
-  console.log(tags.album)
   await sortFile(file, filePath, tags)
+  counter++
+  if (key == array.length-1) {
+    console.log(`Sorted ${counter} mp3 files`)
+  }
 }
 
 const sortFile = async (file, filePath, tags)=>{
-  const artistFolder = `${config.writePath}/${tags.artist}`
-  makeFolderIfNotExists(artistFolder)
-  const albumFolder = `${config.writePath}/${tags.artist}/${tags.album}`
-  makeFolderIfNotExists(albumFolder)
-  console.log(`${albumFolder}${file}`)
-  fs.copyFileSync(filePath, `${albumFolder}/${file}`)
+  let writePath = `${config.writePath}`
+  if (tags.artist) {
+    writePath = `${writePath}/${tags.artist}`
+    makeFolderIfNotExists(writePath)
+  }
+  if (tags.album) {
+    writePath = `${writePath}/${tags.album}`
+    makeFolderIfNotExists(writePath)
+  }
+  fs.copyFileSync(filePath, `${writePath}/${file}`)
+  console.log(`Copied ${file} to ${writePath}`)
 }
 
 const makeFolderIfNotExists = (folderPath)=> {
   if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath)
+    console.log(`Created folder: ${folderPath}`)
   }
 }
 
